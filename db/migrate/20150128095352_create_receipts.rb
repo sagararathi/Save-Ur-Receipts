a975963a2694cc7ccbf17c3e54c31f0c56b7d07c
sagararathi <bravo.sagar@gmail.com>
2015-01-28 10:02:10 -0500

    update receipts migration

--- db/migrate/20150128095352_create_receipts.rb
+++ db/migrate/20150128095352_create_receipts.rb
@@ -1,4 +1,13 @@
 class CreateReceipts < ActiveRecord::Migration
   def change
+    create_table :receipts do |t|
+      t.string :title
+      t.string :content
+      t.string :url
+      t.references :user
+      t.references :category
+
+      t.timestamps
+    end
   end
 end
