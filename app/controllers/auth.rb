012e1436f6ac7d0094c60ee969d6cf1af9d488a9
sagararathi <bravo.sagar@gmail.com>
2015-01-28 11:08:05 -0500

    added get and post route for signup

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -5,11 +5,32 @@ end
 get '/login' do
   erb :'auth/login'
 end
-
+# create post route for user login
 post '/login' do
-  user = User.find_by(username: params[:user][:username])commit
+  user = User.find_by(username: params[:user][:username])
+  if user.try(:authtecation, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/'
+  end
 end
 
 get '/signup' do
-
+# create erb and route for signup
+erb :'auth/signup'
 end
+
+#add post route for signup
+post '/signup' do
+  user =User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    # if time permits will add error handling code
+    redirect '/'
+  end
+end
+
+
