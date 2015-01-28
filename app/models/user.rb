69f94948249506762b56d872d8162fc70c69083b
sagararathi <bravo.sagar@gmail.com>
2015-01-28 10:11:42 -0500

    update user model with association and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # Remember to create a migration!
+  has_many :receipts
+
+  validates :username, :password, :password_confirmation, :email, presence: true
+  validates :username, :email, uniqueness: true 
 end
