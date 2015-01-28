5bea6a21931ca1b8d1b08ef5f416c99d7ab1af6c
sagararathi <bravo.sagar@gmail.com>
2015-01-28 11:38:58 -0500

    added route skeloten for receipts

---
+++ app/controllers/receipt.rb
@@ -0,0 +1,44 @@
+# create route for display all receipts
+get '/receipts' do
+
+end
+# create route for add new receipt
+
+get '/receipts/new' do
+
+end
+
+post '/receipts' do
+
+end
+
+# create route for searching by title
+get '/receipts/title' do
+
+end
+
+# create route for edit
+get '/receipts/:id/edit' do
+
+end
+
+put '/receipts/:id' do
+
+end
+
+# create route to delete specific
+
+delete '/receipts/:id' do
+
+end
+
+#create route for adding category
+
+get '/categories/new' do
+
+end
+
+post '/categories' do
+
+end
+
