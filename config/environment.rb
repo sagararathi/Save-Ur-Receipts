5ce1e226d456ed5998d629dd0fde2b6893fc6af2
sagararathi <bravo.sagar@gmail.com>
2015-01-28 10:51:12 -0500

    add code for user authtecation

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,18 @@ configure do
   enable :sessions
   set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
 
+  #This peace of code will check user authtecation
+
+  register do
+    def auth type
+      condition do
+        unless send("current_#{type}")
+          add_error!("Please Login First")
+          redirect '/login'
+      end
+    end
+  end
+
   # Set the views to
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
